756c7174f49d808194518289719f68e87d624781
Cam <zawacki.cameron@gmail.com>
2014-04-04 14:03:53 -0400

    Added it all back

---
+++ DungeonInfinity/Assets/Notes/Notes.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Notes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	//hsdfhsfu
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ DungeonInfinity/Assets/Scripts/WorldData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldData : MonoBehaviour {
+
+	public static int HEIGHT = 4; // make even
+	public static int WIDTH = 4;
+	public static Hex[,] squares = new Hex[WIDTH, HEIGHT];
+	
+	
+	
+}
